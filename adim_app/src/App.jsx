7abb20015f4b6099e0195ac00f4c05132afc216c
bishwajeet <bishwajeet.kumar@finoit.com>
2025-05-24 01:37:15 +0530

    Fixed route issue

--- adim_app/src/App.jsx
+++ adim_app/src/App.jsx
@@ -7,6 +7,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { SideDrawer, RouterProvider } from "./component/side_drawer/SideDrawer";
 import Login from "../src/page/Login";
 import PrivateRoute from "./routes/PrivateRoute.jsx";
+import Cookies from "js-cookie";
+import { Navigate } from "react-router-dom";
+
 
 function App() {
   return (
