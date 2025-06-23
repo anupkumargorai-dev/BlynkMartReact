e6b843355b80be835c609b9a5d9faa93f8d7dfd6
bishwajeet <bishwajeet.kumar@finoit.com>
2025-06-23 16:44:03 +0530

    Changed base url

--- adim_app/src/services/apiServices.js
+++ adim_app/src/services/apiServices.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 const apiClient = axios.create({
-  baseURL: "http://localhost:4006/api/auth/",
+  baseURL: "https://blynkmartbackend.onrender.com/api/auth/",
   timeout: 10000,
 });
 // https://blynkmartbackend.onrender.com/
 const unauthApiService = axios.create({
-  baseURL: "http://localhost:4006/api/unauth/",
+  baseURL: "https://blynkmartbackend.onrender.com/api/unauth/",
   timeout: 10000,
 });
 
